66ce2863196d79a8e81501c1813c7688558c5424
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-03-31 18:21:55 -0300

    Adição das controllers e views da Categoria e Produto.

--- CaelumEstoque/Controllers/CategoriaController.cs
+++ CaelumEstoque/Controllers/CategoriaController.cs
@@ -1,22 +1,36 @@
-﻿using CaelumEstoque.DAO;
-using CaelumEstoque.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
 namespace CaelumEstoque.Controllers
 {
     public class CategoriaController : Controller
     {
-        // GET: Categoria
+        //
+        // GET: /Produto/
+
         public ActionResult Index()
         {
-            var dao = new CategoriasDAO();
+            CategoriasDAO dao = new CategoriasDAO();
             IList<CategoriaDoProduto> categorias = dao.Lista();
             ViewBag.Categorias = categorias;
             return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(CategoriaDoProduto categoria)
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            dao.Adiciona(categoria);
+            return RedirectToAction("Index");
         }
     }
 }
