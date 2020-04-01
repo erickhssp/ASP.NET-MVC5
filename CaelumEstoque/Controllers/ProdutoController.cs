abe7bb8782fd964c2ccd0daa077bcb00531b3ae0
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-03-31 22:40:12 -0300

    Inclusão de validações ProdutoController e suas respectivas models e views

--- CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -7,27 +7,46 @@ namespace CaelumEstoque.Controllers
     public class ProdutoController : Controller
     {
         // GET: Produto
-                public ActionResult Index()
+        public ActionResult Index()
         {
             var dao = new ProdutosDAO();
             IList<Produto> produtos = dao.Lista();
             ViewBag.Produtos = produtos;
             return View();
         }
+
         [HttpPost]
         public ActionResult Form()
         {
             CategoriasDAO categoriasDAO = new CategoriasDAO();
+            ViewBag.Produto = new Produto();
             ViewBag.Categorias = categoriasDAO.Lista();
             return View();
         }
 
-        [HttpPost]
         public ActionResult Adiciona(Produto produto)
         {
-            ProdutosDAO dao = new ProdutosDAO();
-            dao.Adiciona(produto);
-            return RedirectToAction("Index", "Home");
+            var idDaInformatica = 1;
+            if (produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.InformaticaComPrecoInvalido", "Produtos da categoria informática devem ter preço maior do que 100");
+            }
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                return RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                var categoriasDAO = new CategoriasDAO();
+                ViewBag.Categorias = categoriasDAO.Lista();
+                return View("Form");
+
+            }
+
         }
     }
 }
