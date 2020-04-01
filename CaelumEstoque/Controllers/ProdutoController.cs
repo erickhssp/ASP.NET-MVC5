d869f6262926d904ff808c2ea8003813fc4ee232
Érick do Nascimento Garcia <erickhssp@outlook.com>
2020-04-01 12:17:54 -0300

    Adição de Action Visualiza em Produto para listagem e validações adicionais

--- CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using CaelumEstoque.DAO;
+﻿
+using CaelumEstoque.DAO;
 using CaelumEstoque.Models;
 using System.Collections.Generic;
 using System.Web.Mvc;
@@ -6,27 +7,29 @@ namespace CaelumEstoque.Controllers
 {
     public class ProdutoController : Controller
     {
-        // GET: Produto
+        //
+        // GET: /Produto/
+
         public ActionResult Index()
         {
-            var dao = new ProdutosDAO();
+            ProdutosDAO dao = new ProdutosDAO();
             IList<Produto> produtos = dao.Lista();
-            ViewBag.Produtos = produtos;
-            return View();
+            return View(produtos);
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Produto = new Produto();
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View(categorias);
         }
 
         [HttpPost]
-        public ActionResult Form()
-        {
-            CategoriasDAO categoriasDAO = new CategoriasDAO();
-            ViewBag.Produto = new Produto();
-            ViewBag.Categorias = categoriasDAO.Lista();
-            return View();
-        }
-
         public ActionResult Adiciona(Produto produto)
         {
-            var idDaInformatica = 1;
+            int idDaInformatica = 1;
             if (produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
             {
                 ModelState.AddModelError("produto.InformaticaComPrecoInvalido", "Produtos da categoria informática devem ter preço maior do que 100");
@@ -35,18 +38,23 @@ namespace CaelumEstoque.Controllers
             {
                 ProdutosDAO dao = new ProdutosDAO();
                 dao.Adiciona(produto);
-                return RedirectToAction("Index", "Home");
-
+                return RedirectToAction("Index");
             }
             else
             {
                 ViewBag.Produto = produto;
-                var categoriasDAO = new CategoriasDAO();
+                CategoriasDAO categoriasDAO = new CategoriasDAO();
                 ViewBag.Categorias = categoriasDAO.Lista();
                 return View("Form");
-
             }
+        }
 
+        public ActionResult Visualiza(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            ViewBag.Produto = produto;
+            return View(produto);
         }
     }
 }
